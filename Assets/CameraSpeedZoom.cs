08be003a26a7d5d9a5210be52e7faf4190d1ab3a
Fabrice C <47283393+fabrisek@users.noreply.github.com>
2023-08-06 14:10:36 +0200

    CameraPrefab

--- Assets/CameraSpeedZoom.cs
+++ Assets/CameraSpeedZoom.cs
@@ -7,28 +7,19 @@ public class CameraSpeedZoom : MonoBehaviour
 {
     [SerializeField] ScriptableValueFloat _percentSpeed;
     [SerializeField] float _lerpSpeed = 2.0f; // Vitesse de l'interpolation
-    [SerializeField] float _delayTime = 0.5f; // Délai avant de commencer l'interpolation
-    private float _targetWidth;
-    private float _startDelayTime;
+    private float _targetWidth; // Valeur de largeur cible pour l'interpolation
     private CinemachineFollowZoom _followZoom;
 
     void Start()
     {
         _followZoom = GetComponent<CinemachineFollowZoom>();
         _targetWidth = _followZoom.m_Width;
-        _startDelayTime = Time.time;
     }
 
     void Update()
     {
         float newTargetWidth = _percentSpeed.Value;
-
-        // Démarrer l'interpolation après le délai
-        if (Time.time - _startDelayTime >= _delayTime)
-        {
-            _targetWidth = Mathf.Lerp(_targetWidth, newTargetWidth, Time.deltaTime * _lerpSpeed);
-        }
-
+        _targetWidth = Mathf.Lerp(_targetWidth, newTargetWidth, Time.deltaTime * _lerpSpeed);
         _followZoom.m_Width = _targetWidth;
     }
 }

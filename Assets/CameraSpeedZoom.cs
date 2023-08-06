96f2ddec312809c42c6e949b2499bd6236db8673
Fabrice C <47283393+fabrisek@users.noreply.github.com>
2023-08-06 13:52:49 +0200

    CameraEffect

--- Assets/CameraSpeedZoom.cs
+++ Assets/CameraSpeedZoom.cs
@@ -1,18 +1,34 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSpeedZoom : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] ScriptableValueFloat _percentSpeed;
+    [SerializeField] float _lerpSpeed = 2.0f; // Vitesse de l'interpolation
+    [SerializeField] float _delayTime = 0.5f; // Délai avant de commencer l'interpolation
+    private float _targetWidth;
+    private float _startDelayTime;
+    private CinemachineFollowZoom _followZoom;
+
     void Start()
     {
-        
+        _followZoom = GetComponent<CinemachineFollowZoom>();
+        _targetWidth = _followZoom.m_Width;
+        _startDelayTime = Time.time;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float newTargetWidth = _percentSpeed.Value;
+
+        // Démarrer l'interpolation après le délai
+        if (Time.time - _startDelayTime >= _delayTime)
+        {
+            _targetWidth = Mathf.Lerp(_targetWidth, newTargetWidth, Time.deltaTime * _lerpSpeed);
+        }
+
+        _followZoom.m_Width = _targetWidth;
     }
 }
